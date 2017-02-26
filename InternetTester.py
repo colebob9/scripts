6f4434c5fc159afe7c6380e95f0eea7962f05d9c
colebob9 <colebob9@gmail.com>
2017-02-25 23:09:02 -0600

    v2 InternetTester

--- InternetTester.py
+++ InternetTester.py
@@ -1,5 +1,5 @@
 """
-InternetTester v1
+InternetTester v2
 Python 3
 colebob9
 
@@ -7,16 +7,29 @@ Infinitely tests for pings during an internet outage until internet is back.
 
 """
 
+# config
+hostname = "google.com"   # site to ping
+waitTime = 15     # time to wait between each ping command
+# end config
+
+
 import time
 import os
+import platform
 
 makeSure = 0
+platformName = platform.system()
+
+
+if platformName == 'Windows':
+        response = os.system("ping " + hostname)
+else:
+        response = os.system("ping -c 2 " + hostname)
+        
 
 while True:
-	hostname = "google.com"
-	response = os.system("ping -c 2 " + hostname)
+        
 	dateTime = time.strftime("%I:%M:%S %p")
-
 	if response == 0:
 		for t in range(5):
 			print('')
@@ -29,4 +42,4 @@ while True:
 	if makeSure == 3:
 		break
 
-	time.sleep(20)
+	time.sleep(waitTime)
